3df15f1391e3c3757388684f6013aed9992e1a36
Bethany Summerson <Bethany Summerson>
2015-12-13 20:29:33 -0500

    added function to switch img onclick

--- scripts/app.js
+++ scripts/app.js
@@ -1,3 +1,11 @@
-var meow = function() {
-	console.log("Hiao niao briaon ciao?");
+var switchClick = function() {
+	var img = document.getElementById("imgPlane");
+
+	console.log(img.src);
+	if (img.src.indexOf("plane1") >= 0) {
+        img.src = "assets/plane2.png";
+    }
+    else {
+        img.src = "assets/plane1.png";
+    }
 };
